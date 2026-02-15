471adfe7eed5f0ce9b055a2d4cc6867bed88acf5
vgbrandon <vgbrandon.dev@gmail.com>
2026-02-15 17:08:45 -0500

    change border

--- lua/config/lazygit.lua
+++ lua/config/lazygit.lua
@@ -5,8 +5,7 @@
 -- Configuración de LazyGit
 vim.g.lazygit_floating_window_winblend = 0 -- Transparencia de la ventana (0-100)
 vim.g.lazygit_floating_window_scaling_factor = 0.9 -- Tamaño de la ventana (0.1-1.0)
---vim.g.lazygit_floating_window_border_chars = { "╭", "─", "─", "│", "╯", "─", "╯", "│" } -- Caracteres del borde
-vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+vim.g.lazygit_floating_window_border_chars = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
 vim.g.lazygit_floating_window_use_plenary = 0 -- Usar plenary para ventana flotante
 vim.g.lazygit_use_neovim_remote = 1 -- Usar neovim-remote para abrir archivos desde lazygit
 vim.g.lazygit_use_custom_config_file_path = 0 -- No usar config personalizado
